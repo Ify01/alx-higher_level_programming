fa1e83544411081cb1a5117826393ddbfe417254
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-12-04 16:17:38 -0800

    matrix

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,10 +2,8 @@
 
 
 def add_tuple(tuple_a=(0, 0), tuple_b=(0, 0)):
-    if len(tuple_a) == 1:
-        tuple_a = tuple_a[0], 0
-    if len(tuple_b) == 1:
-        tuple_b = tuple_b[0], 0
+    tuple_a = tuple_a + (0,) * (2 - len(tuple_a))
+    tuple_b = tuple_b + (0,) * (2 - len(tuple_b))
 
     added_tuple = tuple(x + y for x, y in zip(tuple_a, tuple_b))
     return added_tuple
