bfc83a3ff0172c2e64ea569a2ee5372709cd7202
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-12-04 17:22:38 -0800

    ad

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        formatted_row = map('{:d}'.format, row)
-        print(*formatted_row, sep=' ')
+        print(" ".join('{:d}'.format(column) for column in row))
