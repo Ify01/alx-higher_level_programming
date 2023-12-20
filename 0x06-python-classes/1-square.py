83811e59937f60dabed99e8183db7d0cd09605d7
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2023-12-19 16:21:20 -0800

    square

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+"""Defines a Square class"""
+
 
 class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The side length of the square.
+    """
 
     def __init__(self, size):
+        """Initializes a new Square instance.
+
+        Args:
+            size (int): The side length of the square.
+        """
         self.__size = size
