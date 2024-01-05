5fdff9efec5e8a6c3497dc7e1dc3b212f30c23d7
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2024-01-04 19:13:02 -0800

    real

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,47 +1,49 @@
 #!/usr/bin/python3
-"""Defines a Rectangle class."""
+"""A class that defines a rectangle"""
 
 
 class Rectangle:
-    """Represents a rectangle."""
+    """This class represents a rectangle"""
 
     def __init__(self, width=0, height=0):
-        """Initializes the Rectangle class.
+        """Initialize the rectangle class.
+        
         Args:
-            width: The width of the rectangle.
-            height: The height of the rectangle.
+            width (int): Represents the width of the rectangle.
+            height (int): Represents the height of the rectangle.
+
         Raises:
-            TypeError: If width or height is not an integer.
-            ValueError: If width or height is less than zero.
+            TypeError: If size is not an integer.
+            ValueError: If size is less than zero.
         """
-        self.width = width
-        self.height = height
+        self._width = width
+        self._height = height
 
     @property
     def width(self):
-        """Gets the width attribute."""
-        return self.__width
+        """Get the width attribute."""
+        return self._width
 
     @width.setter
     def width(self, value):
-        """Sets the width attribute."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+        """Set the width attribute."""
+        self._validate_size(value, "width")
+        self._width = value
 
     @property
     def height(self):
-        """Gets the height attribute."""
-        return self.__height
+        """Get the height attribute."""
+        return self._height
 
     @height.setter
     def height(self, value):
-        """Sets the height attribute."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+        """Set the height attribute."""
+        self._validate_size(value, "height")
+        self._height = value
 
+    def _validate_size(self, value, attribute):
+        """Validate that size is an integer and non-negative."""
+        if not isinstance(value, int):
+            raise TypeError(f"{attribute} must be an integer")
+        if value < 0:
+            raise ValueError(f"{attribute} must be >= 0")
