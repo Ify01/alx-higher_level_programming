60649455b8e889d11fb80979cff09855fe71c2d8
Ezeugwu Ifeanyi <ifeanyiezeugwu0@gmail.com>
2024-02-14 18:25:59 -0800

    Sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,6 @@
--- deletes the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 doesn't exist, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
+-- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- Results should display both the score and the name (in this order)
+-- Records should be ordered by score (top first)
+-- The database name will be passed as an argument of the mysql command
 
-DROP DATABASE IF EXISTS hbtn_0c_0;
+SELECT score, name FROM second_table ORDER BY score DESC;
